fa6401d8d91e5d02b8e5188d3367daff99759682
chrisjewell <chrisjames.jewell@gmail.com>
2017-10-17 15:51:06 -0400

    loop change

--- this_american_life/this_american_life/spiders/crawl2.py
+++ this_american_life/this_american_life/spiders/crawl2.py
@@ -12,21 +12,21 @@ class CrawlSpider(scrapy.Spider):
 	def parse(self, response):
 		item = TalTranscriptItem()
 		#for line in response.xpath('//div'):
-		item['episode_id'] = line.xpath('//div[contains(@class, "radio-episode-num")]/text()').extract()
-		item['episode_num_text'] = line.xpath('//div[contains(@class, "radio-wrapper")]/@id').extract()
-		item['radio_date_text'] = line.xpath('//div[contains(@class, "radio-date")]/text()').extract()
-		item['episode_title'] = line.xpath('//h2').xpath('a[contains(@href, *)]/text()').extract()
-		item['begin_timestamp'] = line.xpath('//p[contains(@begin, *)]/@begin').extract()
-		item['speaker_class'] = line.xpath('//div/@class').extract()
-		item['speaker_name'] = line.xpath('//h4/text()').extract()
-		item['line_text'] = line.xpath('//p[contains(@begin, *)]/text()').extract()
-		item['full_audio_link'] = line.xpath('//p[contains(@class, "full-audio")]/text()').extract()
+		item['episode_id'] = response.xpath('//div[contains(@class, "radio-episode-num")]/text()').extract()
+		item['episode_num_text'] = response.xpath('//div[contains(@class, "radio-wrapper")]/@id').extract()
+		item['radio_date_text'] = response.xpath('//div[contains(@class, "radio-date")]/text()').extract()
+		item['episode_title'] = response.xpath('//h2').xpath('a[contains(@href, *)]/text()').extract()
+		item['begin_timestamp'] = response.xpath('//p[contains(@begin, *)]/@begin').extract()
+		item['speaker_class'] = response.xpath('//div/@class').extract()
+		item['speaker_name'] = response.xpath('//h4/text()').extract()
+		item['line_text'] = response.xpath('//p[contains(@begin, *)]/text()').extract()
+		item['full_audio_link'] = response.xpath('//p[contains(@class, "full-audio")]/text()').extract()
 		#item['line_id'] = ''
 		#item['transcript_url'] = ''	
 		#item['year'] = ''
 		#item['radio_date_datetime'] = ''
-		#item['episode_hosts'] = line.xpath('//div[contains(@class, "host")]').xpath('h4/text()').extract()
-		#item['act_id'] = line.xpath('//div[contains(@class, "act")]/@id').extract()
+		#item['episode_hosts'] = response.xpath('//div[contains(@class, "host")]').xpath('h4/text()').extract()
+		#item['act_id'] = response.xpath('//div[contains(@class, "act")]/@id').extract()
 		yield item
 
 	#	def parse_dir_contents(self, response):
