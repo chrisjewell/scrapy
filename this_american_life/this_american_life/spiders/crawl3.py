5d48f9ddbfc985a400ee873dd31c570024ab7dba
chrisjewell <chrisjames.jewell@gmail.com>
2017-10-17 17:00:24 -0400

    updates

---
+++ this_american_life/this_american_life/spiders/crawl3.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from this_american_life.items import TalTranscriptItem
+
+class CrawlSpider(scrapy.Spider):
+	name = "transcript2"
+	allowed_domains = ["https://www.thisamericanlife.org/radio-archives/episode/1/transcript"]
+	start_urls = (
+		'https://www.thisamericanlife.org/radio-archives/episode/1/transcript',
+	)
+
+	def parse(self, response):
+		item = TalTranscriptItem()
+		for line in response.xpath('//p'):
+			item['begin_timestamp'] = line.xpath('./@begin').extract_first()
+			item['line_text'] = line.xpath('./text()').extract_first()
+			
+			
+			
+			
+		# for line in response.xpath('//div'):
+			# item['full_audio_link'] = line.xpath('.//p[contains(@class, "full-audio")]/text()').extract()
+			# item['episode_id'] = line.xpath('.//div[contains(@class, "radio-episode-num")]/text()').extract()
+			# item['episode_num_text'] = line.xpath('.//div[contains(@class, "radio-wrapper")]/@id').extract()
+			# item['radio_date_text'] = line.xpath('.//div[contains(@class, "radio-date")]/text()').extract()
+			# item['episode_title'] = line.xpath('.//h2').xpath('a[contains(@href, *)]/text()').extract()
+			# item['speaker_class'] = line.xpath('.//div/@class').extract()
+			# item['speaker_name'] = line.xpath('.//h4/text()').extract()
+			# #item['line_id'] = ''
+			# #item['transcript_url'] = ''	
+			# #item['year'] = ''
+			# #item['radio_date_datetime'] = ''
+			# #item['episode_hosts'] = line.xpath('.//div[contains(@class, "host")]').xpath('h4/text()').extract()
+			# #item['act_id'] = line.xpath('.//div[contains(@class, "act")]/@id').extract()
+			
+		return item
+
+	#	def parse_dir_contents(self, response):
+
